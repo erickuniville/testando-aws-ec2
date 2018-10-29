cc62c188a65eda24956073a0c0852c58478f3e7e
erick.budal <erick.budal@fluig.com>
2018-10-29 15:25:14 -0200

    commit 3

--- src/main/java/ErickTestando.java
+++ src/main/java/ErickTestando.java
@@ -1,7 +1,10 @@
 import com.amazonaws.util.EC2MetadataUtils;
-import static com.amazonaws.util.EC2MetadataUtils.InstanceInfo;
 
+import java.util.List;
 import java.util.Scanner;
+
+import static com.amazonaws.util.EC2MetadataUtils.InstanceInfo;
+import static com.amazonaws.util.EC2MetadataUtils.NetworkInterface;
 
 public class ErickTestando {
     public static void main(String[] args) {
@@ -11,30 +14,51 @@ public class ErickTestando {
         while(true) {
             try {
                 String option = input.nextLine().toLowerCase();
-                String result = "";
                 if (option.equals("id")) {
-                    result = EC2MetadataUtils.getInstanceId();
+                    System.out.println(EC2MetadataUtils.getInstanceId());
                 } else if (option.equals("ip")) {
-                    result = EC2MetadataUtils.getInstanceInfo().getPrivateIp();
+                    System.out.println(EC2MetadataUtils.getInstanceInfo().getPrivateIp());
                 } else if (option.equals("inf")) {
                     InstanceInfo instanceInfo = EC2MetadataUtils.getInstanceInfo();
-                    result += "privateIp: " + instanceInfo.getPrivateIp() + "\n" +
-                              "instanceId: " + instanceInfo.getInstanceId() + "\n" +
-                              "accountId: " + instanceInfo.getAccountId() + "\n" +
-                              "architecture: " + instanceInfo.getArchitecture() + "\n" +
-                              "availabilityZone: " + instanceInfo.getAvailabilityZone() + "\n" +
-                              "imageId: " + instanceInfo.getImageId() + "\n" +
-                              "instanceType: " + instanceInfo.getInstanceType() + "\n" +
-                              "kernelId: " + instanceInfo.getKernelId() + "\n" +
-                              "pendingTime: " + instanceInfo.getPendingTime() + "\n" +
-                              "ramdiskId: " + instanceInfo.getRamdiskId() + "\n" +
-                              "region: " + instanceInfo.getRegion() + "\n" +
-                              "version: " + instanceInfo.getVersion() + "\n";
+                    System.out.println("privateIp: " + instanceInfo.getPrivateIp());
+                    System.out.println("instanceId: " + instanceInfo.getInstanceId());
+                    System.out.println("accountId: " + instanceInfo.getAccountId());
+                    System.out.println("architecture: " + instanceInfo.getArchitecture());
+                    System.out.println("availabilityZone: " + instanceInfo.getAvailabilityZone());
+                    System.out.println("imageId: " + instanceInfo.getImageId());
+                    System.out.println("pendingTime: " + instanceInfo.getPendingTime());
+                    System.out.println("ramdiskId: " + instanceInfo.getRamdiskId());
+                    System.out.println("region: " + instanceInfo.getRegion());
+                    System.out.println("version: " + instanceInfo.getVersion());
+                    System.out.println("version: " + instanceInfo.getVersion());
+                    for (String billingProduct : instanceInfo.getBillingProducts()) {
+                        System.out.println("    billingProduct: " + billingProduct);
+                    }
+                    for (String devProductCode : instanceInfo.getDevpayProductCodes()) {
+                        System.out.println("    devProductCode: " + devProductCode);
+                    }
+                } else if (option.equals("net")) {
+                    List<NetworkInterface> networkInterces = EC2MetadataUtils.getNetworkInterfaces();
+                    for (NetworkInterface net : networkInterces) {
+                        System.out.println("hostname: " + net.getHostname());
+                        System.out.println("macAdress: " + net.getMacAddress());
+                        System.out.println("ownerId: " + net.getOwnerId());
+                        System.out.println("profile: " + net.getProfile());
+                        System.out.println("publicHostname: " + net.getPublicHostname());
+                        System.out.println("subnetId: " + net.getSubnetId());
+                        System.out.println("subnetIPv4CidrBlock: " + net.getSubnetIPv4CidrBlock());
+                        System.out.println("vpcId: " + net.getVpcId());
+                        System.out.println("vpcIPv4CidrBlock: " + net.getVpcIPv4CidrBlock());
+                        for (String ipV4 : net.getLocalIPv4s()) {
+                            System.out.println("    localIPv4: " + ipV4);
+                        }
+                        for (String securityGroup : net.getSecurityGroups()) {
+                            System.out.println("    securityGroup: " + securityGroup);
+                        }
+                    }
                 } else {
-                    result = "ERROR";
+                    System.out.println("Esse comando não existe!");
                 }
-
-                System.out.println("RESULT: " + result);
             }
             catch (Exception ex) {
                 ex.printStackTrace();
