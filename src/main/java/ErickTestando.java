638017007ce086872d9bfb73beccfba2f9f593a0
erick.budal <erick.budal@fluig.com>
2018-10-29 16:34:34 -0200

    minimal 4

--- src/main/java/ErickTestando.java
+++ src/main/java/ErickTestando.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 public class ErickTestando {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.println("============= TESTANDO INSTANCE-ID =============");
+
         while (true) {
-            try {
-                input.nextLine();
-                System.out.println(EC2MetadataUtils.getInstanceId());
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            new Scanner(System.in).nextLine();
+            System.out.println(EC2MetadataUtils.getInstanceId());
         }
     }
 }
